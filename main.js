08f80f0f9cf66c773a2e5dc224aaf84ee2bcc8dc
Gleb <150589066+Gleb25869@users.noreply.github.com>
2024-07-23 21:05:27 +0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+let user_warr = prompt('Чтоб попасть на сайт, введите пароль');
+
+while (user_warr != 'J205491161680_User') {
+        let user_warr = prompt('Чтоб попасть на сайт, введите пароль');
+
+        if (user_warr == 'J205491161680_User') break;
+}
